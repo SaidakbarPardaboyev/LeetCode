3d0853ba38d742467560c76d01d5d7407948e63b
Saidakbar_Pardaboyev <147316135+SaidakbarPardaboyev@users.noreply.github.com>
2023-12-19 18:54:25 +0300

    completed 1232 essignment

--- 1207.py
+++ 1207.py
@@ -1,11 +1,24 @@
-def uniqueOccurrences(arr: list[int]) -> bool:
-    tem = set(arr)
+def checkStraightLine(coordinates: list[list[int]]) -> bool:
+    if len(coordinates) <= 2:
+        return True
 
-    ls = list()
+    if coordinates[1][0] == coordinates[0][0]:
+        x_val = coordinates[0][0]
+        for i in range(2, len(coordinates)):
+            if coordinates[i][0] != x_val:
+                return False
+        return True
 
-    for i in tem:
-        ls.append(arr.count(i))
+    slope = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0])
 
-    return len(ls) == len(set(ls))
+    for i in range(2, len(coordinates)):
+        if coordinates[i][0] - coordinates[0][0] == 0:
+            return False
 
-print(uniqueOccurrences([1,2,2,1,1,3]))
+        current_slope = (coordinates[i][1] - coordinates[0][1]) / (coordinates[i][0] - coordinates[0][0])
+        if current_slope != slope:
+            return False
+
+    return True
+
+print(checkStraightLine([[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]))
