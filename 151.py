2f9ef8a940f5c4176d5fc8a96c9cd7222b095dcc
Saidakbar_Pardaboyev <147316135+SaidakbarPardaboyev@users.noreply.github.com>
2024-01-30 18:56:39 +0300

    Completed 151 essignment

---
+++ 151.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()
+
+        s = s.split()
+
+        return " ".join(s[::-1])
+    
+print(Solution().reverseWords("  hello world  "))
