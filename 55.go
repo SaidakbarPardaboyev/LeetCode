f9607b89aab997480f1934ac6bc5f7e1fcafffde
Saidakbar_Pardaboyev <saidakbarpardaboyev103@gmail.com>
2024-04-24 12:09:14 +0500

    Completed 55 essignment

---
+++ 55.go
@@ -0,0 +1,25 @@
+func canJump(nums []int) bool {
+	canJumbToPerIndexs := []bool{true}
+
+	for i := len(nums) - 2; i >= 0; i-- {
+		// check if there is a path from current index to the end
+		isTherePath := false
+		for j := 0; j < len(canJumbToPerIndexs) && j < nums[i]; j++ {
+			if canJumbToPerIndexs[j] {
+				isTherePath = true
+				break
+			}
+		}
+
+		// if there is a path, add true to the from of the list
+		// because by this true you will know you can to go the end
+		// by visiting the current index
+		if isTherePath {
+			canJumbToPerIndexs = append([]bool{true}, canJumbToPerIndexs...)
+		} else {
+			canJumbToPerIndexs = append([]bool{false}, canJumbToPerIndexs...)
+		}
+	}
+
+	return canJumbToPerIndexs[0]
+}
