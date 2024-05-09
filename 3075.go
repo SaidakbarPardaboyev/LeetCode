f88f19d13c10de8de8d206447c23e0a51a0fdb1e
Saidakbar_Pardaboyev <pardaboyevsaidakbar103@gmail.com>
2024-05-09 09:18:24 +0500

    Completed 3075 essignment

--- 3075.go
+++ 3075.go
@@ -1,20 +1,61 @@
-import "sort"
-
 func maximumHappinessSum(happiness []int, k int) int64 {
-	sort.Slice(happiness, func(i, j int) bool {
-		return happiness[i] > happiness[j]
-	})
+	MergeSort(happiness, 0, len(happiness)-1)
 
 	maximumHappiness := 0
 	happinessDecreasingBy := 0
+
 	for i := 0; i < len(happiness) && k > 0; i++ {
 		if happiness[i]-happinessDecreasingBy >= 0 {
 			maximumHappiness += happiness[i] - happinessDecreasingBy
 		}
-
 		happinessDecreasingBy++
 		k--
 	}
 
 	return int64(maximumHappiness)
+}
+
+func MergeSort(nums []int, start, end int) {
+	if start >= end {
+		return
+	}
+
+	mid := (start + end) / 2
+	MergeSort(nums, start, mid)
+	MergeSort(nums, mid+1, end)
+
+	Merge(nums, start, mid, end)
+}
+
+func Merge(nums []int, start, mid, end int) {
+	left := make([]int, mid-start+1)
+	copy(left, nums[start:mid+1])
+	right := make([]int, end-mid)
+	copy(right, nums[mid+1:end+1])
+
+	l := 0
+	r := 0
+	n := start
+
+	for l < len(left) && r < len(right) {
+		if left[l] > right[r] {
+			nums[n] = left[l]
+			l++
+		} else {
+			nums[n] = right[r]
+			r++
+		}
+		n++
+	}
+
+	for l < len(left) {
+		nums[n] = left[l]
+		n++
+		l++
+	}
+	for r < len(right) {
+		nums[n] = right[r]
+		n++
+		r++
+	}
 }
