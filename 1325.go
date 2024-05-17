262b6162885f4cb02c76d6b4649b141014c509cc
Saidakbar_Pardaboyev <pardaboyevsaidakbar103@gmail.com>
2024-05-17 11:32:14 +0500

    Completed 1325 assignment

---
+++ 1325.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+	var dfs func(node *TreeNode) *TreeNode
+
+	dfs = func(node *TreeNode) *TreeNode {
+		if node == nil {
+			return nil
+		}
+
+		node.Left = dfs(node.Left)
+		node.Right = dfs(node.Right)
+
+		if node.Left == nil && node.Right == nil && node.Val == target {
+			return nil
+		}
+		return node
+	}
+	dfs(root)
+	if root.Val == target && root.Left == nil && root.Right == nil {
+		return nil
+	}
+	return root
+}
